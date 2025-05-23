2c38af7bfc0655cf34c4009cf68f8a4038054479
nishi-maina <31101993jjjrrr@gmail.com>
2025-05-23 09:17:37 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYP html>
+<html lang="ja">
+
+<head>
+  <meta charset = "UTF-8">
+  <title>kadai_007</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    
+    //値を代入する
+    $personal_date = ['name' => 'onion', 'price'=> 200, 'weight'=> 160];
+
+    //出力する
+    print_r($personal_date);
+    ?>
+  </p>
+</body>
+
+</html>
